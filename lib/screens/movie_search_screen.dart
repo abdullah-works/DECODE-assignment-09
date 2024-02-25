bb7a610a15fd3d975fcf7509ce0352adb77a10b4
abdullah-works <abdullah.worke@gmail.com>
2024-02-25 22:48:06 +0500

    API logic added

--- lib/screens/movie_search_screen.dart
+++ lib/screens/movie_search_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:omdb_api_app/models/movie_blueprint.dart';
+import 'package:omdb_api_app/utility/constants.dart';
+import 'package:http/http.dart' as http;
 
 class MovieSearchScreen extends StatefulWidget {
   const MovieSearchScreen({super.key});
@@ -9,11 +15,43 @@ class MovieSearchScreen extends StatefulWidget {
 
 class _MovieSearchScreenState extends State<MovieSearchScreen> {
   TextEditingController movieC = TextEditingController();
+  StreamController streamController = StreamController();
+  Movie? movie;
+
+  @override
+  void initState() {
+    streamController.add(Constants.Initial);
+    super.initState();
+  }
 
   @override
   void dispose() {
     movieC.dispose();
     super.dispose();
+  }
+
+  Future<Movie?> getMovieDetails(String movieName) async {
+    String url = 'https://www.omdbapi.com/?t=$movieName&apikey=c4cc281e';
+
+    streamController.add(Constants.Loading);
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+
+      if (jsonResponse['Response'] == 'True') {
+        streamController.add(Constants.Found);
+
+        movie = Movie.fromJson(jsonResponse);
+      } else {
+        streamController.add(Constants.NotFound);
+      }
+    } else {
+      streamController.add(Constants.Error);
+    }
+
+    return movie;
   }
 
   @override
@@ -39,6 +77,7 @@ class _MovieSearchScreenState extends State<MovieSearchScreen> {
                 label: Text('Search'),
                 hintText: 'Type a movie name',
                 border: OutlineInputBorder(),
+                suffixIcon: Icon(Icons.search),
               ),
             ),
             const SizedBox(height: 8),
@@ -48,6 +87,7 @@ class _MovieSearchScreenState extends State<MovieSearchScreen> {
                 ElevatedButton(
                     onPressed: () {
                       movieC.clear();
+                      streamController.add(Constants.Initial);
                     },
                     child: const Text('Clear')),
                 const SizedBox(width: 8),
@@ -65,11 +105,17 @@ class _MovieSearchScreenState extends State<MovieSearchScreen> {
 
                         return;
                       }
+                      getMovieDetails(movieName);
                     },
                     child: const Text('Search')),
               ],
             ),
-            // TODO: will display movie details here.
+            StreamBuilder(
+                stream: streamController.stream,
+                builder: ((context, snapshot) {
+                  // will handle all the events cases
+                  return const SizedBox.shrink();
+                })),
           ],
         ),
       ),
---
+++ lib/utility/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const Initial = 'Initial';
+  static const Loading = 'Loading';
+  static const Found = 'Found';
+  static const NotFound = 'Not Found';
+  static const Error = 'Error';
+}
